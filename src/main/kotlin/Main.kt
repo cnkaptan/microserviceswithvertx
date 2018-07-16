cf49839a779c1bde9f98288fc20cfada1133eb21
Cihan Kaptan <cihan.kaptan@modera.ee>
2018-07-16 12:22:19 +0300

    Database implemented

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,23 @@
+import com.fasterxml.jackson.databind.util.JSONPObject
 import io.vertx.core.AsyncResult
 import io.vertx.core.MultiMap
 import io.vertx.core.Vertx
 import io.vertx.core.eventbus.Message
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
+import io.vertx.ext.jdbc.JDBCClient
 import io.vertx.ext.web.Route
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.core.json.JsonObject
 import io.vertx.kotlin.core.json.get
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.experimental.CompletableDeferred
+import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.launch
 
 fun main(args: Array<String>) {
@@ -37,4 +42,63 @@ fun RoutingContext.respond(responseBody: String ="", status: Int = 200){
     this.response()
             .setStatusCode(status)
             .end(responseBody)
+}
+
+// We now will create JDBCClient by using this configuration code:
+fun CoroutineVerticle.getDbClient(): JDBCClient {
+    val postgreSQLClientConfig = JsonObject(
+            "url" to "jdbc:postgresql://${Config.Db.host}:5432/${Config.Db.database}",
+            "username" to Config.Db.username,
+            "password" to Config.Db.password)
+    return JDBCClient.createShared(vertx, postgreSQLClientConfig)
+}
+
+/**
+ * Here, we chose an extension function that will work on all CoroutineVerticles.
+ * To simplify working with the JDBCClient, we'll add a method called query() to it:
+ */
+
+fun JDBCClient.query(q: String, vararg params: Any): Deferred<JsonObject> {
+    val deferred = CompletableDeferred<JsonObject>()
+    this.getConnection { conn ->
+        conn.handle({
+            result().queryWithParams(q, params.toJsonArray()) { res ->
+                res.handle({
+                    deferred.complete(res.result().toJson())
+                }, {
+                    deferred.completeExceptionally(res.cause())
+                })
+            }
+        }, {
+            deferred.completeExceptionally(conn.cause())
+        })
+    }
+
+    return deferred
+}
+
+
+// We'll also add the toJsonArray() method since that's what our JDBCClient works with:
+private fun <T> Array<T>.toJsonArray(): JsonArray{
+    val json = JsonArray()
+    for (e in this){
+        json.add(e)
+    }
+    return json
+}
+
+
+/**
+ * Note here how Kotlin generics are being used to simplify the conversion while staying type-safe.
+ *
+ * And we'll add a handle() function, which will provide us with a simple API to handle asynchronous errors:
+ */
+
+inline fun <T> AsyncResult<T>.handle(success: AsyncResult<T>.()-> Unit, failure: () -> Unit){
+    if (this.succeeded()){
+        success()
+    }else{
+        this.cause().printStackTrace()
+        failure()
+    }
 }
--- src/main/kotlin/ServerVerticle.kt
+++ src/main/kotlin/ServerVerticle.kt
@@ -1,13 +1,18 @@
 import com.sun.management.jmx.Trace.send
 import io.netty.util.internal.SocketUtils.accept
 import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.Router.router
 import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.core.json.get
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 
+// We use multiline strings here, with | and trimMargin() to re-align them.
+private val insert = """insert into cats (name, age)
+            |values (?, ?::integer) RETURNING *""".trimMargin()
 class ServerVerticle: CoroutineVerticle() {
 
     override suspend fun start() {
@@ -17,14 +22,17 @@ class ServerVerticle: CoroutineVerticle() {
 
     private fun router(): Router {
         val router = Router.router(vertx)
+        val dbClient = getDbClient()
 
         router.route("/*").handler(BodyHandler.create())
         router.get("/alive").asyncHandler {
+            val dbAlive = dbClient.query("select true as alive")
             // Some response comes here
             // We now can use any suspending function in this context
             val json = json {
                 obj (
-                        "alive" to true
+                        "alive" to true,
+                        "db" to dbAlive.await()["rows"]
                 )
             }
             it.respond(json.toString())
@@ -39,6 +47,17 @@ class ServerVerticle: CoroutineVerticle() {
         val router = Router.router(vertx)
         router.post("/cats").asyncHandler { ctx ->
             // Some code of adding a cat comes here
+            val db = getDbClient()
+            router.post("/cats").asyncHandler { ctx ->
+                db.queryWithParams(insert, ctx.bodyAsJson.toCat()) {
+                    it.handle({
+                        // We''l always have one result here, since it's our row
+                        ctx.respond(it.result().rows[0].toString(), 201)
+                    },{
+                        ctx.respond(status = 500)
+                    })
+                }
+            }
         }
         router.get("/cats").asyncHandler { ctx ->
         }
@@ -47,4 +66,13 @@ class ServerVerticle: CoroutineVerticle() {
         }
         return router
     }
+}
+
+/**
+ * Notice that we have two different versions of this here. One refers to the inner scope of the apply() function.
+ * The other refers to the outer scope of the toCat() function. To refer to outer scopes, we use the @scopeName notation.
+ */
+private fun JsonObject.toCat() = JsonArray().apply {
+    add(this@toCat.getString("name"))
+    add(this@toCat.getInteger("age"))
 }
